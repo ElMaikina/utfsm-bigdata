77271f75271203db3b696fdd057b840195c2866c
ElMaikina <mikesoto890@gmail.com>
2025-06-15 18:26:59 -0400

    arreglar nombres de variables en code-008

--- avanzado/code/code-008.py
+++ avanzado/code/code-008.py
@@ -16,13 +16,13 @@ df = spark.read.parquet(input_path)
 print(f"Total registros cargados: {df.count()}")
 
 # Asegurarse que las coordenadas están en float
-df = df.withColumn("gal_l", col("gal_l").cast("float"))
-df = df.withColumn("gal_b", col("gal_b").cast("float"))
+df = df.withColumn("Galactic right ascension", col("Galactic right ascension").cast("float"))
+df = df.withColumn("Galactic declination", col("Galactic declination").cast("float"))
 df = df.withColumn("exposition_time", col("exposition_time").cast("float"))
 
 # Crear segmentos de 10 grados
-df = df.withColumn("ra_bin", floor(col("gal_l") / 10) * 10 + 5)  # Centro del bin
-df = df.withColumn("dec_bin", floor(col("gal_b") / 10) * 10 + 5)
+df = df.withColumn("ra_bin", floor(col("Galactic right ascension") / 10) * 10 + 5)  # Centro del bin
+df = df.withColumn("dec_bin", floor(col("Galactic declination") / 10) * 10 + 5)
 
 # Agrupar por bin e instrumento
 df_grouped = df.groupBy("ra_bin", "dec_bin", "instrument").agg(
@@ -31,8 +31,8 @@ df_grouped = df.groupBy("ra_bin", "dec_bin", "instrument").agg(
 )
 
 # Renombrar columnas para mayor claridad
-df_grouped = df_grouped.withColumnRenamed("ra_bin", "gal_l") \
-                       .withColumnRenamed("dec_bin", "gal_b")
+df_grouped = df_grouped.withColumnRenamed("ra_bin", "Galactic right ascension") \
+                       .withColumnRenamed("dec_bin", "Galactic declination")
 
 # Guardar resultado
 df_grouped.write.mode("overwrite").parquet(output_path)
