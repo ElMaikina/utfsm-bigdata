0b1842784f010c5fb924cd06eb563ac16221d2db
ElMaikina <mikesoto890@gmail.com>
2025-06-16 13:02:10 -0400

    crear funcion para el unix time manual

--- avanzado/code/code-005.py
+++ avanzado/code/code-005.py
@@ -1,8 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType
-from pyspark.sql.functions import col, split, unix_timestamp
-import datetime
-import time
+from pyspark.sql.types import StructType, StructField, StringType, LongType
+from pyspark.sql.functions import col, split, udf
 
 # Bucket personal
 bucket = "204303630-inf356"
@@ -77,8 +75,45 @@ df_split = df_filtered \
     .withColumn("dec_deg", split(col("declination"), ":").getItem(0)) \
     .withColumn("dec_min", split(col("declination"), ":").getItem(1)) \
     .withColumn("dec_sec", split(col("declination"), ":").getItem(2)) \
-    .withColumn("template_start_unix", unix_timestamp(col("template_start"))) \
+    .withColumn("date", split(col("template_start"), "T").getItem(0)) \
+    .withColumn("time", split(col("template_start"), "T").getItem(1)) \
     .withColumn("exposition_time", col("exposition_time").cast("float"))
+
+# Separa las fechas en meses, dias, horas, etc.
+df_split = df_split \
+    .withColumn("year", split(col("date"), "-").getItem(0).cast("long")) \
+    .withColumn("month", split(col("date"), "-").getItem(1).cast("long")) \
+    .withColumn("day", split(col("date"), "-").getItem(2).cast("long")) \
+    .withColumn("hours", split(col("time"), ":").getItem(0).cast("long")) \
+    .withColumn("minutes", split(col("time"), ":").getItem(1).cast("long")) \
+    .withColumn("seconds", split(col("time"), ":").getItem(2).cast("long")) \
+
+# Funcion lambda que determina si un ano es bisiesto
+leap = lambda y: (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0)
+
+# Funcion lambda que devuelve la cantidad de dias de cada mes
+days_per_month = lambda y: [0, 31, 28 + int(leap(y)), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+# Calcula la cantidad de anos bisiestos desde 1970 hasta el ano indicado
+sum_leap_years = lambda y: sum([i for i in range(1970, y) if leap(i) == True])
+
+# Suma los dias totales de cada ano desde 1970 hasta el ano indicado
+sum_days_in_years = lambda y: 365 * (y - 1970) + sum_leap_years(y)
+
+# Suma los dias totales de cada mes desde enero hasta el mes indicado
+sum_days_in_months = lambda m: sum([i for i in range(0, m+1) if days_per_month(i) != 0])
+
+# Obtiene los dias totales desde 1970 hasta la fecha actual
+sum_total_days = lambda y, m, d: sum_days_in_years(y) + sum_days_in_months(m) + d
+
+# Funcion que obtiene el tiempo en formato unix de forma manual
+manual_unix_time =  lambda Y, M, D, h, m, s: sum_total_days(Y,M,D) * 86400 + h * 3600 + m * 60 + s
+
+# Funcion que puede aplicarse a las columnas de el dataframe de PySpark
+df_manual_unix_time = udf(manual_unix_time,LongType())
+
+# Finalmente aplica la funcion para obtener el tiempo en formato unix
+df_split = df_split.withColumn("template_start_unix", df_manual_unix_time(col("year"), col("month"), col("day"), col("hours"), col("minutes"), col("seconds")))
 
 # Selecciona solo las columnas utiles
 df_final = df_split.select(
